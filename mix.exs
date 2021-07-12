49fe97de828d6cd5fb7f82798c4dcfc0c867e83f
Liang <lzh.scut@hotmail.com>
2021-07-12 18:22:21 +0800

    降低elixir依赖版本
    降低elixir依赖版本

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule JiebaEx.MixProject do
     [
       app: :jieba,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       source_url: "https://github.com/mjason/jieba_ex",
